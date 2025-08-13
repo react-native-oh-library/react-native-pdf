7995b69798de87b95751f65c4a9892a643d5f568
quguiren <quguiren1@huawei-partners.com>
2025-08-13 17:25:14 +0800

    修复pdf bug
    Signed-off-by: quguiren <quguiren1@huawei-partners.com>

--- index.js
+++ index.js
@@ -433,7 +433,12 @@ export default class Pdf extends Component {
             console.log("===react-native-pdf style: " + JSON.stringify(this.props.style));
             return (
             <View style={[{ overflow: 'hidden' }, this.props.style]}>
-                <RNPDFPdfView {...this.props} path={this.state.path} onChange={this._onChange}  />
+                <RNPDFPdfView
+                    {...this.props}
+                    style={{...this.props.style, width: '100%'}}
+                    path={this.state.path}
+                    onChange={this._onChange}
+                />
             </View>
             );
         } else {
